f793a3c1f5c863cdc6e092413445bab3ac21ba8d
Adam Rokah <adamrokah96@gmail.com>
2026-03-18 14:47:47 +0000

    created open3dmm file structure

--- scripts/build_zod_moe_dataset.py
+++ scripts/build_zod_moe_dataset.py
@@ -93,7 +93,7 @@ SOURCE_PARQUET = Path(
 )
 
 
-OUT_ROOT = Path("/home/edgelab/zod_moe")
+OUT_ROOT = Path("/mnt/ZOD_clone_2018_scaleout_zenseact/zod_moe")
 OUT_IMAGES = OUT_ROOT / "bev_images"
 OUT_LIDAR = OUT_ROOT / "lidar"
 OUT_CALIBS = OUT_ROOT / "calibs"
