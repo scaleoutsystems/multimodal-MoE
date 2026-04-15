1ee9e3ffa8910a02deaec093c016509783c45a75
MeluXina user <u103958@login02.meluxina.lxp.lu>
2026-04-15 19:36:34 +0200

    updated filenames for clarity

--- mmdetection3d/projects/BEVFusion/bevfusion/moe_bev/__init__.py
+++ mmdetection3d/projects/BEVFusion/bevfusion/moe_bev/__init__.py
@@ -2,8 +2,8 @@ from .losses import group_balance_loss, importance_loss, load_loss
 from .routing import BEVSummaryHead, ContextEncoder, TopkGate
 from .bev_experts import BEVResidualExpert, make_bev_experts
 from .bev_moe import BEVMoEBlock
-from .fusion_moe import JointModalityExpert, JointModalityMoEBlock
-from .modality_moe import ModalitySpecificMoEBlock
+from .joint_modality_moe import JointModalityExpert, JointModalityMoEBlock
+from .modality_specific_moe import ModalitySpecificMoEBlock
 
 __all__ = [
     'importance_loss', 'load_loss', 'group_balance_loss',
